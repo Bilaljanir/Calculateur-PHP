578150fd9decf17e4687069aedc5dc0f499f21dc
bilaljanir <bilaljanir@gmail.com>
2023-02-02 09:42:18 +0100

    Add file PHP and htl css

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="main.css">
+<div class="titre">
+    <title>CalculateurPHP</title>
+</div>
+</head>
+<body>
+<h1>Calculateur de moyenne CFC </h1>
+
+</body>
+</html>
