5e1d1fb93e7080b2e9debefadcbc203a39745020
bilaljanir <bilaljanir@gmail.com>
2023-02-06 13:46:48 +0100

    add file for the second page grade

---
+++ public/grade.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="grade.css">
+
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Calculateur de moyenne </h1>
+<div class="line-container">
+    <hr class="line"/>
+</div>
+<div class="ecolepro">
+    <h2>Ecole pro</h2>
+</div>
+<div class="Cours Iter">
+    <h2>Cours inter</h2>
+</div>
+<div class="compétancedebase">
+    <h2>Compétance de base élargie</h2>
+</div>
+<div class="cultureG">
+    <h2>Culture G</h2>
+</div>
+<div class="tpi">
+    <h2>TPI</h2>
+</div>
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -39,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($username === 'user1' && $password === 'pass1' ||
         $username === 'user2' && $password === 'pass2') {
-        header('Location: login.php');
+        header('Location: grade.php');
         exit;
     } else {
         echo '<p>Invalid username or password.</p>';
